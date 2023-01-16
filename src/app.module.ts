86c8658d47bcbaf727e6434d4673f70233b85141
gabrielppd77 <gabrielppd77@outlook.com>
2023-01-16 19:37:11 -0300

    commit to git

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+
+import { DatabaseModule } from './infra/database/database.module';
+import { HttpModule } from './infra/http/http.module';
+
+@Module({
+  imports: [HttpModule, DatabaseModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/app/use-cases/get-recipient-notifications.spec.ts
@@ -0,0 +1,34 @@
+import { makeNotification } from '@test/factories/notification-factory';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { GetRecipientNotifications } from './get-recipient-notifications';
+
+describe('Count recipients notification', () => {
+  it('should be able to count recipient notifications', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const getRecipientNotifications = new GetRecipientNotifications(
+      notificationsRepository,
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-1' }),
+    );
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-1' }),
+    );
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-2' }),
+    );
+
+    const { notifications } = await getRecipientNotifications.execute({
+      recipientId: 'recipient-1',
+    });
+
+    expect(notifications).toHaveLength(2);
+    expect(notifications).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ recipientId: 'recipient-1' }),
+        expect.objectContaining({ recipientId: 'recipient-1' }),
+      ]),
+    );
+  });
+});
